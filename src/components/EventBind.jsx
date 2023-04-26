a2f70f70a41324e83ac19106df6f34c010b3be27
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-04-26 17:42:46 +0530

    event binding

--- src/components/EventBind.jsx
+++ src/components/EventBind.jsx
@@ -8,22 +8,40 @@ class EventBind extends Component {
             message: "Hi",
         };
 
+        // -> binding in the constructor
         // this is the recommended apporoach in the official documentation
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick() {
+        if (this.state.message === 'Welcome'){
+            this.setState({
+                message: "Hi",
+            });
+            return;
+        }
         this.setState({
             message: "Welcome",
         });
     }
+
+    // --> class property as arrow functions
+    // handleClick = () => {
+    //     this.setState({
+    //         message: "Welcome",
+    //     });
+    // };
 
     render() {
         return (
             <>
                 <h1>{this.state.message}</h1>
                 {/* these are teh possible solutions for event binding */}
+
+                {/* --> arrow function in render */}
                 {/* <button onClick={() => this.handleClick()}> Send </button> */}
+
+                {/* --> binding in render */}
                 {/* <button onClick={this.handleClick.bind(this)}> Send </button> */}
                 <button onClick={this.handleClick}> Send </button>
             </>
